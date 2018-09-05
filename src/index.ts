80fd2d7cfc4d2a8e8fe8857067eaf6ed0a9f13f4
krzysztof-grzybek <krzysztof.grzybek89@gmail.com>
2018-09-05 21:16:58 +0200

    basic input compilation

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,62 @@
 import * as ts from "typescript";
 
-console.log('test');
+function createCompilerHost(fileName, fileContent): ts.CompilerHost {
+  return {
+    fileExists: () => true,
+    getCanonicalFileName: (filename: string) => filename,
+    getCurrentDirectory: () => '',
+    getDefaultLibFileName: () => 'lib.d.ts',
+    getDirectories: (_path: string) => [],
+    getNewLine: () => '\n',
+    getSourceFile: (filenameToGet: string) => {
+      return filenameToGet === fileName ? ts.createSourceFile(filenameToGet, fileContent, ts.ScriptTarget.ES5, true) : undefined;
+    },
+    readFile: () => undefined,
+    useCaseSensitiveFileNames: () => true,
+    writeFile: () => undefined
+  };
+}
 
-// function compile(fileNames: string[], options: ts.CompilerOptions): void {
-//   let program = ts.createProgram(fileNames, options);
-//   let emitResult = program.emit();
+function compile(options: ts.CompilerOptions, fileContent): void {
+  const fileNameMain = 'main.ts';
 
-//   let allDiagnostics = ts
-//     .getPreEmitDiagnostics(program)
-//     .concat(emitResult.diagnostics);
+  const compilerHost: ts.CompilerHost = createCompilerHost(fileNameMain, fileContent);
+  let program = ts.createProgram([fileNameMain], options, compilerHost);
+  let emitResult = program.emit();
 
-//   allDiagnostics.forEach(diagnostic => {
-//     if (diagnostic.file) {
-//       let { line, character } = diagnostic.file.getLineAndCharacterOfPosition(
-//         diagnostic.start!
-//       );
-//       let message = ts.flattenDiagnosticMessageText(
-//         diagnostic.messageText,
-//         "\n"
-//       );
-//       console.log(
-//         `${diagnostic.file.fileName} (${line + 1},${character + 1}): ${message}`
-//       );
-//     } else {
-//       console.log(
-//         `${ts.flattenDiagnosticMessageText(diagnostic.messageText, "\n")}`
-//       );
-//     }
-//   });
+  let allDiagnostics = ts
+    .getPreEmitDiagnostics(program)
+    .concat(emitResult.diagnostics);
 
-//   let exitCode = emitResult.emitSkipped ? 1 : 0;
-//   console.log(`Process exiting with code '${exitCode}'.`);
-// }
+  allDiagnostics.forEach(diagnostic => {
+    if (diagnostic.file) {
+      let { line, character } = diagnostic.file.getLineAndCharacterOfPosition(
+        diagnostic.start!
+      );
+      let message = ts.flattenDiagnosticMessageText(
+        diagnostic.messageText,
+        "\n"
+      );
+      console.log(
+        `${diagnostic.file.fileName} (${line + 1},${character + 1}): ${message}`
+      );
+    } else {
+      console.log(
+        `${ts.flattenDiagnosticMessageText(diagnostic.messageText, "\n")}`
+      );
+    }
+  });
 
-// compile(['fixture.ts'], {
-//   noEmitOnError: true,
-//   noImplicitAny: true,
-//   target: ts.ScriptTarget.ES5,
-//   module: ts.ModuleKind.CommonJS
-// });
+  let exitCode = emitResult.emitSkipped ? 1 : 0;
+  console.log(`Process exiting with code '${exitCode}'.`);
+}
+
+document.getElementById('input-code').addEventListener('input', (e) => {
+  console.clear();
+  compile({
+    noEmitOnError: true,
+    noImplicitAny: true,
+    target: ts.ScriptTarget.ES5,
+    module: ts.ModuleKind.CommonJS
+  }, (<HTMLTextAreaElement>e.target).value);
+});
