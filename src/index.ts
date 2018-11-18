1062ae4d05cb01427bdcc34c3de2713b6aef2de4
Krzysztof Grzybek <krzysztof.grzybek89@gmail.com>
2018-11-18 15:01:04 +0100

    fix scrollbar

--- src/index.ts
+++ src/index.ts
@@ -6,14 +6,20 @@ import { App } from './app';
 import '../css/styles.scss';
 
 const DEFAULT_CODE =
-`class MyCustomClass {
-  constructor(private something: string) {
-    this.init();
+`function getIdentifierToken(): SyntaxKind.Identifier | KeywordSyntaxKind {
+  // Reserved words are between 2 and 11 characters
+  // long and start with a lowercase letter
+  const len = tokenValue.length;
+  if (len >= 2 && len <= 11) {
+    const ch = tokenValue.charCodeAt(0);
+    if (ch >= CharacterCodes.a && ch <= CharacterCodes.z) {
+      const keyword = textToKeyword.get(tokenValue);
+      if (keyword !== undefined) {
+        return token = keyword;
+      }
+    }
   }
-  
-  init() {
-    this.myString = 'asdf';
-  }
+  return token = SyntaxKind.Identifier;
 }
 `;
 
